23b3f1ce70569d2b6c21d998d13457a1a64366d9
Jude Sajith <judesajith.aj@gmail.com>
2023-04-07 03:22:21 +0530

    ssss

--- pages/Audio_Transcription.py
+++ pages/Audio_Transcription.py
@@ -1,15 +1,11 @@
-import os
 import streamlit as st
 from pathlib import Path
 from llama_index import download_loader
 AudioTranscriber = download_loader("AudioTranscriber")
 
 # Create directory if it doesn't exist
-if not os.path.exists("audio"):
-    os.makedirs("audio")
-
-# Get absolute path of "audio" directory
-audio_dir = os.path.abspath("audio")
+audio_dir = Path("audio")
+audio_dir.mkdir(exist_ok=True)
 
 # Streamlit app code
 st.title("Audio Uploader")
@@ -20,25 +16,30 @@ audio_file = st.file_uploader("Upload audio file", type=["mp3", "wav"])
 # Save audio file to "audio" directory
 if audio_file is not None:
     file_name = audio_file.name
-    file_path = os.path.join("audio", file_name)
+    file_path = audio_dir / file_name
     with open(file_path, "wb") as f:
         f.write(audio_file.getbuffer())
     st.success(f"File saved to: {file_path}")
 
 # Show list of available audio files
-audio_files = os.listdir("audio")
+audio_files = [file.name for file in audio_dir.glob("*")]
 if len(audio_files) > 0:
     st.write("Available audio files:")
     selected_file = st.selectbox("", audio_files)
-    file_path = os.path.join("audio", selected_file)
+    file_path = audio_dir / selected_file
     st.write(f"File path: {file_path}")
     loader = AudioTranscriber()
     audio = st.audio(file_path)
-    st.write(f"Audio directory path: {audio_dir}")
-    documents = loader.load_data(file=Path("/app/lawfirm/audio/mypodcast_64kb.mp3"))
+
+    # Transcribe audio file
+    documents = loader.load_data(file=file_path)
+    transcripts = loader.transcribe(documents)
+    st.write("Transcripts:")
+    for transcript in transcripts:
+        st.write(transcript)
 
 else:
     st.warning("No audio files found. Please upload.")
 
 # Show path of "audio" directory
-
+st.write(f"Audio directory path: {audio_dir}")
