2ece2abf0c3b25b98ac827a2ea77b07effc6e3de
Jude Sajith <judesajith.aj@gmail.com>
2023-04-07 13:48:24 +0530

    sssss

---
+++ pages/Scrape_Web_content.py
@@ -0,0 +1,51 @@
+import streamlit as st
+from pathlib import Path
+import llama_index
+from llama_index import download_loader, GPTSimpleVectorIndex, Document
+import os
+
+AudioTranscriber = download_loader("AudioTranscriber")
+BeautifulSoupWebReader = download_loader("BeautifulSoupWebReader")
+
+web_dir = Path("Web")
+web_dir.mkdir(exist_ok=True)
+
+# Create directory if it doesn't exist
+# Streamlit app code
+st.title("Chat with Knowledge from Scraped Web Content")
+
+with st.expander("Upload Audio"):
+# Input field for the URL to be loaded in the data
+    url_input = st.text_input("Enter the URL to be scraped")
+    scrape_url = st.button("Scrape URL")
+
+    if scrape_url:
+        loader = BeautifulSoupWebReader()
+        documents = loader.load_data(urls=[url_input])
+        st.success(f"URL content scraped successfully!")
+
+    # documents = loader.load_data(file=pat)
+    index = GPTSimpleVectorIndex.from_documents(documents)
+    index_file_path = web_dir / f"{selected_file}.json"
+        
+    # Save the index to the data directory with the same name as the PDF
+    index.save_to_disk(index_file_path)
+        st.success(f"{selected_file} 's Index created successfully!")
+    else:
+        st.warning("No audio files found. Please upload.")
+
+try:
+    index_files = [file.name for file in web_dir.glob(f"{selected_file}.json")]
+    selected_index_file = st.selectbox("Select a Scraped Website Index to ask questions ", index_files)
+    index_file_path = web_dir / selected_index_file
+    index = GPTSimpleVectorIndex.load_from_disk(index_file_path)
+except NameError:
+    st.warning("No index files found for this audio file. Please transcribe the audio file first.")
+
+inp = st.text_input("Ask question")
+ask = st.button("Submit")
+
+if ask:
+    res = index.query(inp)
+    st.write(res)
+    pass
