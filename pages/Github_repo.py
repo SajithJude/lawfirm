6869ece8097f1fe60eb47b011284685a65f373a6
Jude Sajith <judesajith.aj@gmail.com>
2023-04-07 16:42:40 +0530

    ss

---
+++ pages/Github_repo.py
@@ -0,0 +1,33 @@
+import os
+import streamlit as st
+from llama_index import download_loader
+from llama_index.readers.llamahub_modules.github_repo import GithubRepositoryReader, GithubClient
+
+download_loader("GithubRepositoryReader")
+
+# Define Streamlit input components for each input parameter
+owner = st.text_input("Enter the owner of the GitHub repository")
+repo = st.text_input("Enter the name of the GitHub repository")
+filter_directories = st.multiselect("Select directories to include", options=["gpt_index", "docs"])
+filter_file_extensions = st.multiselect("Select file extensions to include", options=[".py"])
+verbose = st.checkbox("Verbose mode")
+concurrent_requests = st.slider("Select number of concurrent requests", min_value=1, max_value=20, value=10)
+
+# Load data from the GitHub repository using the selected input parameters
+github_client = GithubClient(os.getenv("GITHUB_TOKEN"))
+loader = GithubRepositoryReader(
+    github_client,
+    owner=owner,
+    repo=repo,
+    filter_directories=(filter_directories, GithubRepositoryReader.FilterType.INCLUDE),
+    filter_file_extensions=(filter_file_extensions, GithubRepositoryReader.FilterType.INCLUDE),
+    verbose=verbose,
+    concurrent_requests=concurrent_requests,
+)
+
+docs_branch = loader.load_data(branch="main")
+docs_commit = loader.load_data(commit="a6c89159bf8e7086bea2f4305cff3f0a4102e370")
+
+# Display the extra_info for each doc
+for doc in docs:
+    st.write(doc.extra_info)
