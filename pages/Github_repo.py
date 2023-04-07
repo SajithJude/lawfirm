8a80f39b42c13b8eadf9e57b087a1890432a3cff
Jude Sajith <judesajith.aj@gmail.com>
2023-04-07 21:51:35 +0530

    xxx

--- pages/Github_repo.py
+++ pages/Github_repo.py
@@ -8,7 +8,7 @@ download_loader("GithubRepositoryReader")
 # Define Streamlit input components for each input parameter
 owner = st.text_input("Enter the owner of the GitHub repository")
 repo = st.text_input("Enter the name of the GitHub repository")
-filter_directories = st.multiselect("Select directories to include", options=["gpt_index", "docs"])
+filter_directories = st.multiselect("Select directories to include", options=["pages", "docs"])
 filter_file_extensions = st.multiselect("Select file extensions to include", options=[".py"])
 verbose = st.checkbox("Verbose mode")
 concurrent_requests = st.slider("Select number of concurrent requests", min_value=1, max_value=20, value=10)
@@ -25,8 +25,8 @@ loader = GithubRepositoryReader(
     concurrent_requests=concurrent_requests,
 )
 
-docs_branch = loader.load_data(branch="main")
-docs_commit = loader.load_data(commit="a6c89159bf8e7086bea2f4305cff3f0a4102e370")
+docs_branch = loader.load_data(branch="master")
+# docs_commit = loader.load_data(commit="a6c89159bf8e7086bea2f4305cff3f0a4102e370")
 
 # Display the extra_info for each doc
 for doc in docs:
