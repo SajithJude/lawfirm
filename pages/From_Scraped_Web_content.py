1301524d58e613a7779fe662d92f2ad686178e98
Jude Sajith <judesajith.aj@gmail.com>
2023-04-09 17:32:58 +0530

    ssss

--- pages/From_Scraped_Web_content.py
+++ pages/From_Scraped_Web_content.py
@@ -34,6 +34,8 @@ with st.expander("Input URL"):
         index.save_to_disk(f"url.json")
  
     # b ir / selected_index_file
+llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-davinci-003", max_tokens=1024))
+service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor)
 
 index = GPTSimpleVectorIndex.load_from_disk(f"url.json", service_context=service_context)
 if index:
