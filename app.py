660d4e5bf6e8d8a9b5401743973f3b2a8426593a
Jude Sajith <judesajith.aj@gmail.com>
2023-04-09 17:57:35 +0530

    ssss

--- app.py
+++ app.py
@@ -2,14 +2,4 @@ import streamlit as st
 
 st.title('OpenAI-powered Chatbot Demo Apps')
 
-st.write('Welcome to my portfolio of demo apps for building OpenAI-powered chatbots that are connected to custom data sources! With my apps, you can easily create chatbots that can handle a variety of different data sources, including uploaded PDF documents, uploaded PPTX files, audio files, and even scraped data from the internet.')
-
-st.write('My apps use the power of OpenAI to create chatbots that are able to understand natural language and respond to users in a conversational manner. This means that users can ask questions and get answers from the chatbot just as they would from a human expert.')
-
-st.write('One of the key features of my apps is their ability to connect to custom data sources. This means that you can upload your own PDFs, PPTX files, or audio files and have the chatbot use that data to answer user questions. For example, if you have a PDF document containing information about your company\'s products or services, the chatbot can use that information to answer questions from potential customers.')
-
-st.write('In addition to connecting to custom data sources, my apps can also scrape data from the internet. This means that you can create chatbots that can answer questions about a wide range of topics, even if you don\'t have all the information in-house.')
-
-st.write('My portfolio of demo apps includes a range of different examples, from simple chatbots that can answer basic questions to more complex chatbots that can handle more advanced queries. Each app is designed to be easy to use and customize, so you can quickly create a chatbot that meets your needs.')
-
-st.write('If you\'re looking to create a chatbot that can handle a variety of different data sources and respond to users in a conversational manner, my portfolio of demo apps is the perfect place to start. With my apps, you can quickly and easily create powerful chatbots that will help you engage with your customers and provide them with the information they need. So why wait? Start exploring my portfolio of demo apps today and see what you can create!')
+st.write('Build custom chatbots with my OpenAI-powered demo apps that connect to various data sources, including PDFs, PPTX files, audio files, and scraped internet data. Respond to users in a conversational manner, allowing you to engage with your customers and provide them with the information they need. Explore my portfolio of easy-to-use demo apps today!')
