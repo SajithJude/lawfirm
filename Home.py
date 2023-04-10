ba72b554813da4d27a5e3da47f165a2e3a571a34
Jude Sajith <judesajith.aj@gmail.com>
2023-04-10 06:32:51 +0530

    ssss

--- Home.py
+++ Home.py
@@ -12,52 +12,52 @@ with col1:
     st.image('images/drive.png', width=100)
     st.write('Google Drive')
 with col2:
-    st.image('images/gmail.png', width=100)
+    st.image('images/drive.png', width=100)
     st.write('Gmail')
 with col3:
-    st.image('images/calander.png', width=100)
+    st.image('images/drive.png', width=100)
     st.write('Google Calander Loader')
 
 col4, col5, col6 = st.columns(3)
 with col4:
-    st.image('images/confluence.jpg', width=100)
+    st.image('images/drive.jpg', width=100)
     st.write('Confluence ')
 with col5:
-    st.image('images/mongodb.png', width=100)
+    st.image('images/drive.png', width=100)
     st.write('MongoDB Connector')
 with col6:
-    st.image('postgresql_loader.png', width=100)
+    st.image('images/drive.png', width=100)
     st.write('PostgreSQL ')
 
 col7, col8, col9 = st.columns(3)
 with col7:
-    st.image('mysql_loader.png', width=100)
+    st.image('images/drive.png', width=100)
     st.write('MySQL ')
 with col8:
-    st.image('bigquery_loader.png', width=100)
+    st.image('images/drive.png', width=100)
     st.write('BigQuery ')
 with col9:
-    st.image('redshift_loader.png', width=100)
+    st.image('images/drive.png', width=100)
     st.write('Redshift ')
 
 col10, col11, col12 = st.columns(3)
 with col10:
-    st.image('snowflake_loader.png', width=100)
+    st.image('images/drive.png', width=100)
     st.write('Snowflake ')
 with col11:
-    st.image('oracle_loader.png', width=100)
+    st.image('images/drive.png', width=100)
     st.write('Oracle ')
 with col12:
-    st.image('sqlite_loader.png', width=100)
+    st.image('images/drive.png', width=100)
     st.write('SQLite ')
 
 col13, col14 ,col15 = st.columns(3)
 with col13:
-    st.image('Notion_loader.png', width=100)
+    st.image('images/drive.png', width=100)
     st.write('Notion ')
 with col14:
-    st.image('dynamodb_loader.png', width=100)
+    st.image('images/drive.png', width=100)
     st.write('DynamoDB ')
 with col15:
-    st.image('AWSS3.png', width=100)
+    st.image('images/drive.png', width=100)
     st.write('AWS S3 ')
