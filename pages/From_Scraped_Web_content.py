c89179d0bf35e35763161cdfe8ffa06daf1ab0c9
Jude Sajith <judesajith.aj@gmail.com>
2023-04-10 06:16:49 +0530

    sss

--- pages/From_Scraped_Web_content.py
+++ pages/From_Scraped_Web_content.py
@@ -11,6 +11,8 @@ BeautifulSoupWebReader = download_loader("BeautifulSoupWebReader")
 web_dir = Path("Web")
 web_dir.mkdir(exist_ok=True)
 
+if "scrapeIndex" not in st.session_state:
+    st.session_state.scrapeIndex = ""
 # Create directory if it doesn't exist
 # Streamlit app code
 st.title("Chat with Knowledge from Scraped Web Content")
@@ -29,22 +31,21 @@ with st.expander("Input URL"):
         service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor)
 
 
-        index = GPTSimpleVectorIndex.from_documents(documents, service_context=service_context)
+        scrapeIndex = GPTSimpleVectorIndex.from_documents(documents, service_context=service_context)
+        st.session_state.scrapeIndex = scrapeIndex
         # name = web_dir / url_input
-        index.save_to_disk(f"url.json")
+        # index.save_to_disk(f"url.json")
  
     # b ir / selected_index_file
-llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-davinci-003", max_tokens=1024))
-service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor)
 
-index = GPTSimpleVectorIndex.load_from_disk(f"url.json", service_context=service_context)
-if index:
-    st.success("Index Loaded from repository successfully")
+# index = GPTSimpleVectorIndex.load_from_disk(f"url.json", service_context=service_context)
+# if index:
+#     st.success("Index Loaded from repository successfully")
 
 inp = st.text_input("Ask question")
 ask = st.button("Submit")
 
 if ask:
-    res = index.query(inp)
+    res = st.session_state.scrapeIndex.query(inp)
     st.write(res)
     pass
