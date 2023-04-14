0498af13897890efcee81cb06d2d8e0b741a44ec
Jude Sajith <judesajith.aj@gmail.com>
2023-04-15 00:05:56 +0530

    sssss

---
+++ pages/Whatsapp.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from pathlib import Path
+from llama_index import download_loader
+from tempfile import NamedTemporaryFile
+
+st.set_option('deprecation.showfileUploaderEncoding', False)
+
+# Title
+st.title("WhatsApp Chat Analyzer")
+
+# File uploader
+uploaded_file = st.file_uploader("Upload your WhatsApp chat .txt file", type="txt")
+
+if uploaded_file is not None:
+    with NamedTemporaryFile(delete=False, suffix=".txt") as tmp:
+        tmp.write(uploaded_file.getvalue())
+        tmp_path = Path(tmp.name)
+
+    # Load and process the data
+    WhatsappChatLoader = download_loader("WhatsappChatLoader")
+    loader = WhatsappChatLoader(path=str(tmp_path))
+    documents = loader.load_data()
+
+    # Display the results
+    st.write("Loaded chat data:")
+    st.write(documents)
+
+    # Clean up temporary file
+    tmp_path.unlink()
+else:
+    st.warning("Please upload a .txt file to analyze WhatsApp chats.")
