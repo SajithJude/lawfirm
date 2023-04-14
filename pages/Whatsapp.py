6200923b659f4a9dd185311e448866adbdadf832
Jude Sajith <judesajith.aj@gmail.com>
2023-04-15 00:30:32 +0530

    ssss

--- pages/Whatsapp.py
+++ pages/Whatsapp.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from pathlib import Path
-from llama_index import download_loader, GPTSimpleVectorIndex
+from llama_index import download_loader, GPTSimpleVectorIndex, SimpleDirectoryReader
 from tempfile import NamedTemporaryFile
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
@@ -9,17 +9,23 @@ st.set_option('deprecation.showfileUploaderEncoding', False)
 st.title("WhatsApp Chat Analyzer")
 
 # File uploader
+# File uploader
 uploaded_file = st.file_uploader("Upload your WhatsApp chat .txt file", type="txt")
 
 if uploaded_file is not None:
+    # Check if "whatsapp" directory exists, and create it if it doesn't
+    data_dir = Path("whatsapp")
+    if not data_dir.exists():
+        data_dir.mkdir()
+    
     with NamedTemporaryFile(delete=False, suffix=".txt") as tmp:
         tmp.write(uploaded_file.getvalue())
         tmp_path = Path(tmp.name)
 
     # Load and process the data
-    WhatsappChatLoader = download_loader("WhatsappChatLoader")
-    loader = WhatsappChatLoader(path=str(tmp_path))
-    documents = loader.load_data()
+    # WhatsappChatLoader = download_loader("WhatsappChatLoader")
+    # loader = WhatsappChatLoader(path=str(tmp_path))
+    documents = SimpleDirectoryReader(str(data_dir)).load_data()
 
     # Display the results
     st.write("Loaded chat data:")
@@ -29,9 +35,8 @@ if uploaded_file is not None:
         st.session_state.intax = intax
         st.success("session state added index")
 
-    
-
     # Clean up temporary file
+    tmp_path.unlink()
 else:
     st.warning("Please upload a .txt file to analyze WhatsApp chats.")
 
